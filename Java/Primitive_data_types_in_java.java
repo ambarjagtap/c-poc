b1a1f2cd0d2cc6b19ff476e8f36d3cd5dac0667c
Ambar Jagtap <88499151+ambarjagtap@users.noreply.github.com>
2021-08-27 19:50:35 +0530

    Create Primitive_data_types_in_java.java

---
+++ Java/Primitive_data_types_in_java.java
@@ -0,0 +1,31 @@
+// Java program to demonstrate primitive data types
+
+
+// create a class
+class Primitve_types{
+    
+    //the main method
+    // access_modifier called_without_object no_return name (command line arguments)
+    public static void main(String[] args){
+        
+        //declaring Character
+        char ch='A';
+        
+        //declaring an int
+        int a=5;
+        
+        //for memory constraints
+        byte b=5;
+        
+        //declaring float using suffix 'f'
+        float num=3.1473f;
+        
+        
+        //printing all variables
+        System.out.println("char ch = "+ch);
+        System.out.println("int a = "+ a);
+        System.out.println("Byte ="+b);
+        System.out.println("float = "+num);
+        
+    }
+}
