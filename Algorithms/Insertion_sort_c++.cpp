c55b7b7a691413feb078dc03f7b83bc1d0e0a0c3
Ambar Jagtap <88499151+ambarjagtap@users.noreply.github.com>
2021-08-31 23:56:05 +0530

    Create Insertion_sort_c++.cpp

---
+++ Algorithms/Insertion_sort_c++.cpp
@@ -0,0 +1,56 @@
+/* Sorting using Insertion Sort 
+
+  *Take minimum element every time from unsorted array and place it in sorted array.
+  *O(n^2)
+
+*/
+
+
+//Program to implement insertion sort 
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+//Function to print array
+void print(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+}
+
+
+//Function to do insertion sorting
+void insertion_sort(int arr[],int n){
+    
+    //iterate for each element
+    for(int i=0;i<n;i++){
+        int min_ind=i;  //assume current 'i' as 'index as min_value_index'
+        
+        //iterate from i+1 to n
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[min_ind])min_ind=j;
+        }
+        
+        //insert min value element at i's place
+        swap(arr[i],arr[min_ind]);
+    }
+}
+
+
+
+int main(){
+    
+    //input array
+    int n;   cin>>n;  int arr[n];   for(int i=0;i<n;i++)cin>>arr[i];
+    
+    
+    //function for insertion sort
+    insertion_sort(arr,n);
+    
+    
+    //function to print array
+    print(arr,n);
+    
+    return 0;
+}
