b87453f72539b49f1761c5edd6175078f8665586
Ambar Jagtap <88499151+ambarjagtap@users.noreply.github.com>
2021-09-01 10:01:41 +0530

    Update Insertion_sort.java

--- Java/Insertion_sort.java
+++ Java/Insertion_sort.java
@@ -40,6 +40,7 @@ class Insertion_sort{
     
     
     //Method to print array
+    // 'static' method doesn't require any object to invoke it
     static void print_array(int arr[],int n){
         for(int i=0;i<n;i++){
             System.out.println(arr[i]+" ");
@@ -63,13 +64,13 @@ class Insertion_sort{
             arr[i]=obj.nextInt();
         }
         
-        //Create a object of class Insertion sort Dynamically
+        //Create a object of class Insertion sort dynamically
         Insertion_sort ob=new Insertion_sort();
         
         //invoke sort method of object of Insertion_sort class
         ob.sort(arr,n);
         
-        //invoke print method without object
+        //invoke print method without object as it is 'static' method
         print_array(arr,n);
     }
 }
