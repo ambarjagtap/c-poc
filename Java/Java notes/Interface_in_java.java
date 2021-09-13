d2a00b32d0d01650a53248cd77a7ed1776cf0299
Ambar Jagtap <88499151+ambarjagtap@users.noreply.github.com>
2021-09-13 22:33:18 +0530

    Update Interface_in_java.java

--- Java/Java notes/Interface_in_java.java
+++ Java/Java notes/Interface_in_java.java
@@ -2,7 +2,7 @@
   Interface :
     - In common language interface can be described as a link between an object and outside world.
       Eg. Object in a class interact with other objects through methods that they expose.
-    - In java, interface is 'group of related methods, but with empty bodies'.
+    - In java, interface is 'group of related methods, but with empty bodies' and 'no impletation'.
     - Its like a design template of 'related methods' which we can include in our program. 
 */
 
@@ -13,6 +13,7 @@
 interface Bicycle{   // collection of related methods
   
   // methods related to bicycle :
+  //NOTE: There is not implementation.
   void cadence(int newvalue);
   void change_gear(int newvalue);
   void speed_up(int increment);
